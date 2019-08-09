a7892d6a547d08a3a2979786f171957b6b6fcdb5
杨刚 <zhanghongqin@interrcs.com>
2019-08-09 18:44:46 +0800

    * init

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"github.com/QN-Resources/server/entity"
+)
+
+
+func main(){
+	fmt.Println("hh")
+
+	root := entity.Instance()
+
+	root.AddTreeNode("name", "path", 0)
+
+	fmt.Printf("root:%v\n",root)
+
+	return
+
+	// 设置 gin 的模式（调试模式：DebugMode, 发行模式：ReleaseMode）
+	gin.SetMode(gin.DebugMode)
+	// 创建一个不包含中间件的路由器
+	r := gin.Default()
+
+	r.Static("/static", "./static")
+	// static icon
+	r.StaticFile("/favicon.ico", "./static/images/favicon.ico")
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+
+
+	// 在8080 端口，启动http服务
+	r.Run(":8090")
+}
---
+++ server/entity/treenode.go
@@ -0,0 +1,91 @@
+package entity
+
+import (
+	"time"
+	"os"
+	"bytes"
+	"encoding/gob"
+	"fmt"
+	"io/ioutil"
+	"strconv"
+)
+
+type TreeNode struct {
+	Id         string `id`
+	Name       string `file name`
+	Path       string `file path`
+	Type       int    `0:dir 1:file`
+	CreateTime time.Time
+	Children   []TreeNode
+}
+
+var root TreeNode
+
+var FILE_PATH = "./data"
+
+func init() {
+	_, err := os.Stat(FILE_PATH)
+	if err == nil {
+		fmt.Println("load")
+		load()
+
+	} else {
+		root = TreeNode{Id: "root", Name: "root", Path: "root", Type: 0, CreateTime: time.Now(), Children: []TreeNode{}}
+		save()
+	}
+
+	fmt.Println("init")
+}
+
+func Instance() *TreeNode {
+	return &root
+}
+
+func (this *TreeNode) FindNodesById(Id string) []TreeNode {
+	if this.Id == Id {
+		return this.Children
+	} else {
+		for _, node := range this.Children {
+			return node.FindNodesById(Id)
+		}
+
+		return make([]TreeNode, 0)
+	}
+}
+
+func (this *TreeNode) AddTreeNode(Name string, Path string, Type int) {
+	node := TreeNode{}
+	node.Id = strconv.FormatInt(time.Now().Unix(), 10)
+	node.Name = Name
+	node.Path = Path
+	node.Type = Type
+	node.CreateTime = time.Now()
+	this.Children = append(this.Children, node)
+	save()
+}
+
+
+
+func save() {
+	var buf bytes.Buffer           // 构建一个buf
+	encode := gob.NewEncoder(&buf) // 构建一个encodeer
+	encode.Encode(root)            // 序列化数据
+
+	file, err := os.OpenFile(FILE_PATH, os.O_CREATE|os.O_RDWR, 0666)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	defer file.Close()      // 关闭文件指针
+	file.Write(buf.Bytes()) // 写入文件
+}
+
+func load() {
+	buf, err := ioutil.ReadFile(FILE_PATH)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	decoder := gob.NewDecoder(bytes.NewReader(buf))
+	decoder.Decode(&root)
+}
